c9ec5267f7445c64b980c54295b05781b2d0d543
srija shenkeshi <srijashenkeshi@gmail.com>
2023-09-21 13:17:07 +0530

    init commit

---
+++ src/test/java/com/srija/springgithubactionsdemo/SpringGithubActionsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srija.springgithubactionsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubActionsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
